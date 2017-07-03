c93724003948da974832d73c989e40ba1c505679
David Marchante <cdmarchante@gmail.com>
2017-07-03 11:48:22 -0400

    Add files via upload

---
+++ 3_js-foundations/5-20.js
@@ -0,0 +1,13 @@
+/*
+ * Programming Quiz: Laugh (5-4)
+ */
+
+var laugh = function(num) {
+    var laughter = "";
+    for (var i = 0; i < num; i++) {
+        laughter += "ha";
+    }
+    return laughter + "!";
+} /* finish the function expression */
+
+return (laugh(10));
