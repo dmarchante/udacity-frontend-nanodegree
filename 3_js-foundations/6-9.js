221f67f52e704e962265ddb97301e1c912194829
David Marchante <cdmarchante@gmail.com>
2017-07-05 11:00:13 -0400

    Add files via upload

---
+++ 3_js-foundations/6-9.js
@@ -0,0 +1,17 @@
+/*
+ * Programming Quiz: Colors of the Rainbow (6-4)
+ * Using only the splice() method, insert the missing colors into the array, and 
+ * remove the color "Blackberry" by following these steps:
+ *
+ * Remove "Blackberry"
+ * Add "Yellow" and "Green"
+ * Add "Purple"
+ *
+ */
+
+var rainbow = ["Red", "Orange", "Blackberry", "Blue"];
+
+// your code goes here
+rainbow.splice(2, 2, "Yellow", "Green", "Blue", "Purple");
+
+console.log(rainbow);
---
+++ 3_js-foundations/7-2.js
@@ -0,0 +1,13 @@
+var sister = {
+    name: "Sarah",
+    age: 23,
+    parents: ["Alice", "Andy"],
+    siblings: ["Julia"],
+    favoriteColor: "purple",
+    pets: true,
+    paintPicture: function() {
+        return "Sarah paints!";
+    }
+};
+
+console.log(sister.paintPicture());
