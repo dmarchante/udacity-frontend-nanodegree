54477387448802b83ba7e7e4d1808738d7b1135c
David Marchante <cdmarchante@gmail.com>
2017-07-05 16:07:33 -0400

    Add files via upload

---
+++ intro-cs/1-22.py
@@ -0,0 +1,8 @@
+# Write python code that defines the variable 
+# age to be your age in years, and then prints 
+# out the number of days you have been alive.
+age = 25
+days_in_year = 365
+age_in_days = age * days_in_year
+
+print age_in_days
